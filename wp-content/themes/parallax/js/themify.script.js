b312bd57a43538013cb0d40795b37e5a3f390fd0
Marty Mavis <marty.mavis@gmail.com>
2016-09-12 07:17:44 -0500

    removing close js

--- wp-content/themes/parallax/js/themify.script.js
+++ wp-content/themes/parallax/js/themify.script.js
@@ -74,11 +74,9 @@ var ThemifyGallery, FixedHeader, ThemifyParallax;
 					}
 
 					// close mobile menu
-					/*
 					if($(window).width() <= 768 && $('#main-nav').is(':visible')){
 						$('#menu-icon').trigger('click');
 					}
-					*/
 				});
 
 				// change hash url
